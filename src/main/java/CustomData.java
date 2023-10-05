617a8a7ed75076b01f1d7fd4defe0fb02e265424
Aayush Singh <aayush.singh@pratishthanventures.com>
2023-10-05 13:14:14 +0530

    final demo

--- src/main/java/CustomData.java
+++ src/main/java/CustomData.java
@@ -1,8 +1,9 @@
 import py4j.GatewayServer;
+import java.util.*;
 
-public class CustomData {
-    private String pythonFunctionName="modify_with_space";
-    private String data="Hi";
+public class CustomData<T> {
+    private String pythonFunctionName="";
+    private T data;
 
     public String getPythonFunctionName() {
         return pythonFunctionName;
@@ -12,21 +13,36 @@ public class CustomData {
         this.pythonFunctionName = pythonFunctionName;
     }
 
-    public String getData() {
+    public T getData() {
         return data;
     }
 
-    public void setData(String data) {
+    public void setData(T data) {
         this.data = data;
     }
 
     public static void main(String[] args) {
         // Create an instance of CustomData
-        CustomData customData = new CustomData();
+        CustomData<List<String>> customData = new CustomData<>();
+        customData.setPythonFunctionName("modify_with_space");
+        customData.setData(List.of("Hi","Hello","Hey"));
         // Start the Py4J gateway server
         GatewayServer gatewayServer = new GatewayServer(customData);
         gatewayServer.start();
         System.out.println("Py4J gateway server started...");
+        while(true){
+            try{
+                Thread.sleep(1);
+                System.out.println("checking");
+                System.out.println(customData.getData());
+            }
+            catch(InterruptedException e){
+                System.exit(0);
+            }
+
+        }
+
+
 
     }
 }
--- src/main/python/python_server.py
+++ src/main/python/python_server.py
@@ -6,32 +6,47 @@ class PythonServer:
         self.java_gateway = java_gateway
         self.java_server = java_gateway.entry_point  # Get the Java entry point
 
+    def convert_java_list_to_python(self, java_list):
+        # Convert Java List to Python list
+        python_list = list(java_list)
+        return python_list
+
+    def convert_python_to_java_list(self, python_list):
+        # Convert Python list to Java ArrayList
+        java_list = self.java_gateway.jvm.java.util.ArrayList()
+        for item in python_list:
+            java_list.add(item)
+        print(type(java_list))
+        return java_list
+
     def modify_with_space(self, data):
-        new_str = "space" + data
-        return new_str
+        python_list= self.convert_java_list_to_python(data)
+        print(type(python_list))
+        python_list.append("hellloooo")
+        print(python_list)
+        original_list=self.convert_python_to_java_list(python_list)
+        return original_list
 
     def changes(self):
         # Get the Python function name from the Java instance
         python_function_name = self.java_server.getPythonFunctionName()
 
-
         print("Original Data:", self.java_server.getData())
+        #print(type(self.java_server.getData()))
         # Call the custom Python function to modify the data
         modified_data = getattr(self, python_function_name)(self.java_server.getData())
 
-        # Update the Java data with the modified data
         self.java_server.setData(modified_data)
 
         # Print the original and modified data
         print("Modified Data:", modified_data)
 
-        #check the original java data
-        #print(self.java_server.getData())
 
 if __name__ == "__main__":
     # Start a Py4J gateway to connect to Java
     gateway = JavaGateway(gateway_parameters=GatewayParameters(auto_field=True))
     py_server = PythonServer(gateway)
+
     py_server.changes()
     # Shutdown the Py4J gateway
     gateway.shutdown()
